bea20661a1aac7b674ba3698c914c2106ad2a24a
5l1a4 <eliandan090@gmail.com>
2022-06-13 20:40:28 -0300

    Se creó el Repositorio del Proyecto

---
+++ src/main/java/com/analistas/projectpdv/ProjectPdvApplication.java
@@ -0,0 +1,13 @@
+package com.analistas.projectpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analistas/projectpdv/ProjectPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analistas.projectpdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
