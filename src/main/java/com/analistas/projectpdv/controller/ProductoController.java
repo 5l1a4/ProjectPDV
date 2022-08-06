17b2f3f11fee1b2bc3322ccb7c681f9d1773f91d
5l1a4 <eliandan090@gmail.com>
2022-08-05 21:07:35 -0300

    Form in function

--- src/main/java/com/analistas/projectpdv/controller/ProductoController.java
+++ src/main/java/com/analistas/projectpdv/controller/ProductoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -39,10 +40,10 @@ public class ProductoController {
         return"productos/form";
     }
     
-    @GetMapping("/guardar")
-    public String guardarProducto(Model model){
+    @PostMapping("/form")
+    public String guardarProducto(Model model, Producto producto){
     
-        
-        return"producto/form";
+        productoService.guardar(producto);
+        return"redirect:/productos/listado";
     }
 }
